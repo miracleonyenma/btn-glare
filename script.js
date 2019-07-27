3b3e3dd1d778502eac7b66064d1c8ac52b855f98
miracleonyenma <miracleficient@gmail.com>
2019-07-27 17:04:45 +0100

    added glare animation

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 var button = document.querySelector("button"),
-glare,
+btncoords = button.getBoundingClientRect(),
+glare = document.querySelector("button .glare"),
+posX, 
+posY;
+
+function follow(e){
+    posX = ((e.clientX - btncoords.left) - 25) / 1 + "px";
+    posY = ((e.clientY - btncoords.top) - 50) / 1 + "px";
+
+    glare.style.transform = "translate(" + posX + "," + posY + ")";
+    console.log("translate(" + posX + "," + posY + ")");
+}
+
+button.addEventListener('mousemove', follow)
